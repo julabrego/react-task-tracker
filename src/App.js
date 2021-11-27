f80ea017b9534225f21572daf5b52f4e38cb3778
Julián Ábrego <abrego.jul@gmail.com>
2021-11-27 16:52:44 -0300

    toggle reminder via http request to json-server

--- src/App.js
+++ src/App.js
@@ -23,6 +23,14 @@ function App() {
     return data;
   };
 
+  // Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
   // Add Task
   const addTask = async (task) => {
     const res = await fetch("http://localhost:5000/tasks", {
@@ -49,10 +57,23 @@ function App() {
   };
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
